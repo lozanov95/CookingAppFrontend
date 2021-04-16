f17583d724df9868f1559e72e5097272a0d68d45
lozanov95 <vasillozanov1@gmail.com>
2021-04-16 19:39:25 +0300

    Added about section

--- src/app.js
+++ src/app.js
@@ -6,14 +6,17 @@ import { createPage } from './views/create.js';
 import { recipesPage } from './views/recipes.js';
 import { deletePage } from './views/delete.js';
 import { detailsPage } from './views/details.js';
+import { aboutPage } from './views/about.js';
 
 const main = document.getElementById('content');
 
 page('/', middleware, homePage);
-page('/recipes', middleware, recipesPage)
-page('/create', middleware, createPage)
-page('/delete/:id', deletePage)
-page('/details/:id', middleware, detailsPage)
+page('/recipes', middleware, recipesPage);
+page('/create', middleware, createPage);
+page('/delete/:id', deletePage);
+page('/details/:id', middleware, detailsPage);
+page('/about', middleware, aboutPage);
+
 page.start();
 
 
--- src/views/about.js
+++ src/views/about.js
@@ -1,6 +1,14 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
+const aboutTemplate = () => html `
+<div class="about-section">
+            <h1>About me</h1>
+            <p>I am Vasil Lozanov and this is a simple CRUD app that I developed.</p>
+            <p>You can reach me on LinkedIn <a href="https://www.linkedin.com/in/vasil-lozanov-5a1b0a18a/">here</a> or you can email me at <b>vasillozanov1@gmail.com</b> .</p>
+            <p>GitHub repo of this application - <a href="https://github.com/lozanov95/CookingAppFrontend">here</a></p>
+        </div>
+`;
 
 export async function aboutPage(ctx) {
-
+    ctx.render(aboutTemplate());
 }
