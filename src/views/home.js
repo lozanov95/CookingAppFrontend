9251aca9e580d1a51f310949b7e23f6dd6177f88
lozanov95 <vasillozanov1@gmail.com>
2021-04-16 19:27:51 +0300

    added home view and css

--- src/views/home.js
+++ src/views/home.js
@@ -1,3 +1,12 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+const homeTemplate = () => html `
+  <div class="home-section">
+            <h1>Welcome to my cooking app!</h1>
+            <p>View all recipes on the following <a href="/recipes">link</a></p>
+            <p>Read more about this application <a href="/about">here</a></p>
+        </div>`
+
 export async function homePage(ctx) {
-    console.log('home Page')
+    ctx.render(homeTemplate())
 }
