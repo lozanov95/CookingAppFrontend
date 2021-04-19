e928e95f24f6c0a991be4ad1924a59ae87b7045e
lozanov95 <vasillozanov1@gmail.com>
2021-04-19 14:26:35 +0300

    refactored the recipes view

--- src/views/recipes.js
+++ src/views/recipes.js
@@ -1,22 +1,5 @@
 import { getRecipes } from '../api/data.js';
-import { html } from '../../node_modules/lit-html/lit-html.js';
-
-const recipesTemplate = (recipes) => html `
-<div class="card-container">
-    ${recipes.map(recipeCard)}
-</div>
-`;
-
-const recipeCard = (recipe) => html `
-<div class="card">
-    <h3 class="card-title">${recipe.name}</h3>
-    <img class="card-image" src="${recipe.image_url}">
-    <div class="card-footer-space-between">
-        <span>Difficulty: ${recipe.difficulty} </span><a href=${`/details/${recipe.id}`} class="card-btn">View</a>
-    </div>
-</div>
-`
-
+import { recipesTemplate } from '../generic-templates/recipe-template.js';
 
 export async function recipesPage(ctx) {
     const recipes = await getRecipes();
